25ebf80efd2874df7dc9f917ce7ad8dafa3e1fa3
LinYuMachi <clyLINYU@hotmail.com>
2023-08-25 12:15:53 -0700

    Added the ability to switch forms when entering new tickets

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -1,11 +1,16 @@
-import { Box, Button, TextField } from "@mui/material";
-import { Formik } from "formik";
-import * as yup from "yup";
-import useMediaQuery from "@mui/material/useMediaQuery";
+import { Box, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import { useState } from "react";
 import Header from "../../components/Header";
+import Form1 from "./form1";
+import Form2 from "./form2";
 
 const Form = (props) => {
-  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const [form, setForm] = useState(1);
+
+  const handleFormChange = (e) => {
+    console.log(e.target.value);
+    setForm(e.target.value);
+  };
 
   const handleFormSubmit = (values) => {
     console.log(values);
@@ -13,159 +18,45 @@ const Form = (props) => {
   };
 
   return (
-    <Box sx={{
-      position: "absolute",
-      top: "50%",
-      left: "50%",
-      transform: "translate(-50%, -50%)",
-      width: "80%",
-      bgcolor: "background.paper",
-      border: "2px solid #000",
-      boxShadow: 24,
-      p: 4,
-    }}>
+    <Box
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "80%",
+        bgcolor: "background.paper",
+        border: "2px solid #000",
+        boxShadow: 24,
+        p: 4,
+      }}
+    >
       <Header title="CREATE TICKET" subtitle="Create a New Ticket" />
-
-      <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={initialValues}
-        validationSchema={checkoutSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <Box
-              display="grid"
-              gap="30px"
-              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-              sx={{
-                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-              }}
-            >
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="First Name"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.firstName}
-                name="firstName"
-                error={!!touched.firstName && !!errors.firstName}
-                helperText={touched.firstName && errors.firstName}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Last Name"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.lastName}
-                name="lastName"
-                error={!!touched.lastName && !!errors.lastName}
-                helperText={touched.lastName && errors.lastName}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Email"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.email}
-                name="email"
-                error={!!touched.email && !!errors.email}
-                helperText={touched.email && errors.email}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Contact Number"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.contact}
-                name="contact"
-                error={!!touched.contact && !!errors.contact}
-                helperText={touched.contact && errors.contact}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="date"
-                label="Sale Date"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.saleDate}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                name="saleDate"
-                error={!!touched.saleDate && !!errors.saleDate}
-                helperText={touched.saleDate && errors.saleDate}
-                sx={{ gridColumn: "span 1" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="date"
-                label="Flight Date"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.flightDate}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                name="flightDate"
-                error={!!touched.flightDate && !!errors.flightDate}
-                helperText={touched.flightDate && errors.flightDate}
-                sx={{ gridColumn: "span 1" }}
-              />
-            </Box>
-            <Box display="flex" justifyContent="end" mt="20px">
-              <Button type="submit" color="secondary" variant="contained">
-                Create New Ticket
-              </Button>
-            </Box>
-          </form>
-        )}
-      </Formik>
+      <FormControl sx={{ mb: 4 }}>
+        <InputLabel id="form-select-label">Form</InputLabel>
+        <Select
+          labelId="form-select-label"
+          id="form-select"
+          value={form}
+          label="Form"
+          onChange={handleFormChange}
+        >
+          <MenuItem value={1}>First Form</MenuItem>
+          <MenuItem value={2}>Second Form</MenuItem>
+        </Select>
+      </FormControl>
+      {(() => {
+        switch (form) {
+          case 1:
+            return <Form1 handleFormSubmit={handleFormSubmit} />;
+          case 2:
+            return <Form2 handleFormSubmit={handleFormSubmit} />;
+          default:
+            return <Form1 handleFormSubmit={handleFormSubmit} />;
+        }
+      })()}
     </Box>
   );
-};
-
-const phoneRegExp =
-  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
-
-const checkoutSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("invalid email").required("required"),
-  contact: yup
-    .string()
-    .matches(phoneRegExp, "Phone number is not valid")
-    .required("required"),
-  saleDate: yup.string().required("required"),
-  flightDate: yup.string().required("required"),
-});
-const initialValues = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  contact: "",
-  saleDate: "",
-  flightDate: "",
 };
 
 export default Form;
