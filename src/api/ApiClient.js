675f7cd773aa4f4ea8268e8f959b4ba1ce382120
Steven Wang <wfcwfc999@berkeley.edu>
2023-08-29 22:13:21 -0700

    Attempt to add file upload

--- src/api/ApiClient.js
+++ src/api/ApiClient.js
@@ -2,22 +2,24 @@ import axios from "axios";
 
 export default class ApiClient {
   client;
+
   constructor() {
     this.client = axios.create({
       baseURL:
-        "https://iw3b7a08zb.execute-api.ap-southeast-2.amazonaws.com/prod",
+          "https://iw3b7a08zb.execute-api.ap-southeast-2.amazonaws.com/prod",
     });
   }
+
   listTestTable() {
     return this.client.get("test/insert").then((resp) => resp.data);
   }
 
   insertTestTable(name) {
     return this.client
-      .post("test/insert", {
-        name: name,
-      })
-      .then((resp) => resp.data);
+        .post("test/insert", {
+          name: name,
+        })
+        .then((resp) => resp.data);
   }
 
   listFlights() {
@@ -26,19 +28,28 @@ export default class ApiClient {
 
   insertFlight(flight) {
     return this.client
-      .post("/flight", {
-        flight: flight,
-      })
-      .then((resp) => resp.data);
+        .post("/flight", {
+          flight: flight,
+        })
+        .then((resp) => resp.data);
   }
 
   updateFlight(flight, updatedFlight) {
     return this.client
-      .put(`/flight/${flight}`, { flight: updatedFlight })
-      .then((resp) => {});
+        .put(`/flight/${flight}`, {flight: updatedFlight})
+        .then((resp) => {
+        });
   }
 
   deleteFlight(flight) {
-    return this.client.delete(`/flight/${flight}`).then((resp) => {});
+    return this.client.delete(`/flight/${flight}`).then((resp) => {
+    });
+  }
+
+  uploadPassportImage(extension, image) {
+    return this.client.post('upload/passport', {
+      "extension": extension,
+      "image": image
+    }).then(resp => resp.data);
   }
 }
--- src/scenes/test/ApiClientTest.jsx
+++ src/scenes/test/ApiClientTest.jsx
@@ -4,12 +4,32 @@ import {Button} from "@mui/material";
 
 export const ApiClientTest = () => {
   const apiClient = useContext(ApiContext);
-  const [name, setName] = useState("")
+  const [name, setName] = useState("");
+
+
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const [isValid, setIsValid] = useState(true);
+
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    if (file && file.type.includes('image') && file.size <= 5000000) {
+      setIsValid(true);
+      setPreviewUrl(URL.createObjectURL(file));
+      const extension = file.name.slice(((file.name.lastIndexOf(".") - 1) >>> 0) + 2);
+      console.log("a");
+      const buffer = await file.arrayBuffer();
+      console.log("b");
+      apiClient.uploadPassportImage(extension, new Int8Array(buffer)).then((resp) => console.log(resp));
+    } else {
+      setIsValid(false);
+    }
+  };
+
   return (
       <>
         <div>
           <Button type="submit" color="secondary" variant="contained" onClick={() => apiClient.listTestTable().then(resp => console.log(resp))}>
-          List Test Table
+            List Test Table
           </Button>
         </div>
         <div>
@@ -18,6 +38,11 @@ export const ApiClientTest = () => {
           <Button type="submit" color="secondary" variant="contained" onClick={() => apiClient.insertTestTable(name).then(resp => console.log(resp))}>
             Insert into TestTable
           </Button>
+        </div>
+        <div>
+          <input type="file" accept=".jpg,.png,.jpeg" onChange={handleFileChange} />
+          {!isValid && <p>Please select a valid image (jpg, png) with size up to 5MB.</p>}
+          {previewUrl && <img src={previewUrl} alt="Selected" style={{ width: '100%' }} />}
         </div>
       </>
   )
